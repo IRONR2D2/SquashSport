3ac959cd6939a0f71116ecb84d5de185212a073d
unknown <arthur.r2@outlook.com>
2020-01-30 18:28:14 -0600

    Se elimino todas los archivos basura

--- js/scoreLogic.js
+++ js/scoreLogic.js
@@ -16,15 +16,9 @@ nuevoArray[1] = new Array(2); //SE DECLARA LAS POSICIONES QUE TENDRA EN ESTE CAS
 
 
 //BOTONES DE MARCADOR
-<<<<<<< HEAD
-const botonA= document.getElementById('A');
-const botonB= document.getElementById('B');
-const botonR= document.getElementById('R');
-=======
 const botonA = document.getElementById('A');
 const botonB = document.getElementById('B');
 const botonR = document.getElementById('R');
->>>>>>> Se agrego un boton para restablecer el marcador
 
 
 //Marcador
@@ -42,17 +36,10 @@ function persona(punto) {
   } else {
     if (punto == 'B') {
       return "Gano jugador 2";//GREGAR BOTON NUEVA PARTIDA :D
-<<<<<<< HEAD
-    }else{
-      if (punto == 'R') {
-        return 3;
-      }else{
-=======
     } else {
       if (punto == 'R') {
         return "Es un reinicio";
       } else {
->>>>>>> Se agrego un boton para restablecer el marcador
         return punto;
       }
     }
@@ -65,17 +52,12 @@ function persona2(punto) {
   } else {
     if (punto == 'B') {
       return 2;
-<<<<<<< HEAD
-    }else{
-      return punto;
-=======
     } else {
       if (punto == 'R') {
         return 3;
       } else {
         return punto;
       }
->>>>>>> Se agrego un boton para restablecer el marcador
     }
   }
 }
@@ -87,38 +69,6 @@ function scoreCompleto(completo) {
     if (playerA == 10) {
       proceso.innerHTML = "Fin del juego gano jugador 1";
       alert("Gano jugador 1!!");
-<<<<<<< HEAD
-      text=" ";
-    }else{
-      if (playerB==10) {
-        proceso.innerHTML = "Fin del juego gano jugador 2";
-        alert("Gano jugador 2!!");
-        text2=" ";
-      }else{}
-    }
-  }else{
-
-    scoreGeneral++;
-
-  if (persona2(completo)==1) {
-    playerA++
-    marcadorA.innerHTML = playerA;
-    proceso.innerHTML = persona(completo);
-
-    nuevoArray[0][scoreGeneral]=playerA;
-    text=" "; //RECORDAR ESTA LINEA ES ARA REINICIAR EL MARCADOR EN SIN QUE SE SUME ASI MISMO
-    //noesun.innerHTML = nuevoArray[0][scoreGeneral];
-  }else{
-    if (persona2(completo)==2) {
-      playerB++
-      marcadorB.innerHTML = playerB;
-      proceso.innerHTML = persona(completo);
-      nuevoArray[1][scoreGeneral]=playerB;
-      text2=" "; //RECORDAR ESTA LINEA ES ARA REINICIAR EL MARCADOR EN SIN QUE SE SUME ASI MISMO
-      //noesun.innerHTML = nuevoArray[1][scoreGeneral];
-    }else{
-      mined.innerHTML = persona2(completo);
-=======
       text = " ";
     } else {
       if (playerB == 10) {
@@ -154,7 +104,6 @@ function scoreCompleto(completo) {
           console.log("COMO EST");
         }
       }
->>>>>>> Se agrego un boton para restablecer el marcador
     }
 
 
@@ -167,21 +116,12 @@ function mostrarMarcdorA(entregaA) {
   var i;
   for (i = 0; i <= limitePuntos; i++) {
 
-<<<<<<< HEAD
-    if (persona2(entregaA)==1) {
-      if (nuevoArray[0][i]==null) {
-        nuevoArray[0][0]= "-"; //Si se deja seleccionado [o] como i este cambiara todos los resultados. PERO SE DEBE MODIFICAR
-      }else{
-        //text = " ";
-        text += "Jugador A: "+ nuevoArray[0][i] +  " Posicion "+i+"<br>"; //DE IGUAL FORMA SE DEBE QUITAR EL +, si lo dejas se suma
-=======
     if (persona2(entregaA) == 1) {
       if (nuevoArray[0][i] == null) {
         nuevoArray[0][0] = "-"; //Si se deja seleccionado [o] como i este cambiara todos los resultados. PERO SE DEBE MODIFICAR
       } else {
         //text = " ";
         text += "Jugador A: " + nuevoArray[0][i] + " Posicion " + i + "<br>"; //DE IGUAL FORMA SE DEBE QUITAR EL +, si lo dejas se suma
->>>>>>> Se agrego un boton para restablecer el marcador
 
       }
     }
@@ -198,14 +138,6 @@ function mostrarMarcdorB(entregaB) {
 
   var i;
   for (i = 0; i <= limitePuntos; i++) {
-<<<<<<< HEAD
-    if (persona2(entregaB)==2) {
-      if (nuevoArray[1][i]==null) {
-          nuevoArray[1][0]= "-";
-        }else{
-          text2 += "Jugador B: "+ nuevoArray[1][i]+  " Posicion "+i+"<br>"; //DE IGUAL FORMA SE DEBE QUITAR EL +, si lo dejas se suma
-        }
-=======
     if (persona2(entregaB) == 2) {
       if (nuevoArray[1][i] == null) {
         nuevoArray[1][0] = "-";
@@ -215,7 +147,6 @@ function mostrarMarcdorB(entregaB) {
     } else {
       //mined2.innerHTML = "vegetaaa!! ";
       mined2.innerHTML = text2;
->>>>>>> Se agrego un boton para restablecer el marcador
     }
   }
   mined2.innerHTML = text2;
@@ -242,31 +173,15 @@ function reiniciarMarcador(regresoP) {
   }
 }
 
-function regresarMarcador(regresoP){
-  if (persona(regresoP)==3) {
-    mined2.innerHTML ="VALOR DE ARRAY "+nuevoArray[0][6];
-    /**for (i = nuevoArray.length; i <= limitePuntos; i++) {
-    }**/
-  }
-}
-
 
 
 function game(punto) {
 
-<<<<<<< HEAD
-    const movidaUser= punto;
-    scoreCompleto(movidaUser);
-    mostrarMarcdorA(movidaUser);
-    mostrarMarcdorB(movidaUser);
-    regresarMarcador(movidaUser);
-=======
   const movidaUser = punto;
   scoreCompleto(movidaUser);
   mostrarMarcdorA(movidaUser);
   mostrarMarcdorB(movidaUser);
   reiniciarMarcador(movidaUser);
->>>>>>> Se agrego un boton para restablecer el marcador
   /**switch (movidaUser) {
     case 'A':
       scoreCompleto(movidaUser);//GANAR
